9bf2baa7863950a274fb4dfe62b1f4f962bd95c4
Unknown <grooney4@gmail.com>
2018-05-06 21:37:11 -0400

    Initial Commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadNextScene", 2f);
+    }
+
+    void LoadNextScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
