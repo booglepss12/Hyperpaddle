fa171ed7cc8e8c1fc1c81e28d6cd62dfd4f2a4fa
Unknown <grooney4@gmail.com>
2018-05-22 21:18:27 -0400

    Added playerpaddle.cs and started script

---
+++ Assets/Scripts/PlayerPaddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaddle : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	void OnMouseDrag()
+	{
+        print("Dragged to" + Input.mousePosition);
+	}
+	
+	void Update () {
+		
+	}
+}
